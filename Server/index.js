ce013d71edd8ce142a004e794dc6c74337823a61
Sharafatali1175 <seebiz.gpt0124.evedev40@gmail.com>
2025-09-14 20:42:03 +0500

    code changing

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,25 @@
+
+require('dotenv').config()
+
+
 let express = require('express');
 let mongoose = require('mongoose');
 const enquiryRouter = require('./App/routes/web/enquiryRoutes');
+let path = require("path");
+const _dirname = path.resolve()
+
 let cors =require('cors')
-require('dotenv').config()
+// require('dotenv').config()
 let app = express();
 app.use(cors())
 app.use(express.json())
 
 //Routes
 app.use('/api/website/enquiry',enquiryRouter)
-
-
+app.use(express.static(path.join(_dirname, "crud-mern-stack-app/dist")));
+app.get("*" , (req,res)=>{
+    res.sendFile(path.resolve(_dirname, "crud-mern-stack-app", "dist", "index.html"))
+})
 
 mongoose.connect(process.env.DBURL)
 .then(()=>{
